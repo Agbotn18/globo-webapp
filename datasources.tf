9f323cacd1cde00c56829a4696b1675b2996f5a7
Agbotn18 <111661840+Agbotn18@users.noreply.github.com>
2024-02-16 09:45:07 -0600

    Update datasources.tf

--- datasources.tf
+++ datasources.tf
@@ -1,7 +1,7 @@
 ##################################################################################
 # DATA SOURCES
 ##################################################################################
-
+/*
 data "aws_ssm_parameter" "amzn2_linux" {
   name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
 }
@@ -10,3 +10,4 @@ data "tfe_outputs" "networking" {
   organization = var.tfe_organization
   workspace    = var.tfe_workspace_name
 }
+*/
