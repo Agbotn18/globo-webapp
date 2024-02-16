202a6ae5d7e00c3244df2f992853213f8bd54a71
Agbotn18 <111661840+Agbotn18@users.noreply.github.com>
2024-02-16 09:46:28 -0600

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,4 @@
+/*
 output "public_lb_dns" {
   value = aws_lb.main.dns_name
 }
@@ -9,3 +10,4 @@ output "webapp_instance0_public_ip" {
 output "private_key_pem" {
   value = nonsensitive(module.ssh_keys.private_key_pem)
 }
+*/
